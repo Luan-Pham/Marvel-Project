462885ba19081133f054741d6629078436733fab
unknown <phamexb@gmail.com>
2022-07-28 19:11:17 -0700

    working search bar and image replacement

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ const PRIVATE_KEY = "52a651da90a98a4375effb2030b8e3e359d9395e";
 var marvelAPI = "https://gateway.marvel.com:443";
 
 function search() {
+  var picture = document.getElementById("thumbnail");
   var searchValue = document.getElementById("searchInput").value;
   console.log(searchValue);
   const ts = Number(new Date());
   const hash = md5(ts + PRIVATE_KEY + PUBLIC_KEY);
-  console.log(hash);
+  // console.log(hash);
   var characterURL =
     "https://gateway.marvel.com:443/v1/public/characters?ts=" +
     ts +
@@ -28,9 +29,45 @@ function search() {
     "&hash=" +
     hash;
 
+  // fetch(characterURL)
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data));
+
   fetch(characterURL)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then(function (data) {
+      console.log(data);
+      console.log(data.data.results[0].thumbnail)
+      setImage(data)
+    });
 }
 
+var setImage = function(data){
+  $("#thumbnail").attr('src', data.data.results[0].thumbnail.path + "/detail.jpg")
+}
+// function searchComics() {
+//   var searchValue = document.getElementById("searchInput").value;
+//   console.log(searchValue);
+//   const ts = Number(new Date());
+//   const hash = md5(ts + PRIVATE_KEY + PUBLIC_KEY);
+//   console.log(hash);
+//   var characterURL =
+//     "https://gateway.marvel.com:443/v1/public/characters/" +
+//     searchValue +
+//     "/comics?ts=" +
+//     ts +
+//     "&apikey=" +
+//     PUBLIC_KEY +
+//     "&hash=" +
+//     hash;
+
+//   fetch(characterURL)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+
+//   const parsedData = JSON.parse(data);
+// }
+
 submitBtn.addEventListener("click", search);
+
+// https://gateway.marvel.com:443/v1/public/characters/Thor/comics?apikey=331a8e964a4e496298bdfa8a074e7db6

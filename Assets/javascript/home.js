82ea421cb9d717fc3e08282f4fda7fdefed8df70
unknown <phamexb@gmail.com>
2022-07-30 17:29:42 -0700

    added scripts to main branch and reorganized files

---
+++ Assets/javascript/home.js
@@ -0,0 +1,55 @@
+var searcher = document.getElementById("search");
+var submitBtn = document.getElementById("searchBtn");
+
+const PUBLIC_KEY = "331a8e964a4e496298bdfa8a074e7db6";
+const PRIVATE_KEY = "52a651da90a98a4375effb2030b8e3e359d9395e";
+
+var marvelAPI = "https://gateway.marvel.com:443";
+
+function search() {
+  let charID = Math.floor(Math.random() * (1009726-1009301) + 1009301);
+  console.log(charID);
+  const ts = Number(new Date());
+  const hash = md5(ts + PRIVATE_KEY + PUBLIC_KEY);
+  console.log(hash)
+  var characterURL =
+    "https://gateway.marvel.com:443/v1/public/characters/"+ charID + "?ts=" +
+    ts +
+    "&apikey=" +
+    PUBLIC_KEY +
+    "&hash=" +
+    hash;
+
+  fetch(characterURL)
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+      console.log(data.data.results[0].thumbnail)
+      console.log(data.data.results[0].id)
+      setCharacter(data)
+      setDetails(data)
+      setComics(data)
+      setWiki(data)
+    });
+}
+search()
+
+var setCharacter = function(data){
+    $("#characters").attr('src', data.data.results[0].thumbnail.path + "/portrait_uncanny.jpg")
+}
+var setDetails = function(data){
+  $('#charDescription').html(data.data.results[0].description)
+}
+
+
+var setComics = function(data){
+  $('#comicsLink').attr('href', data.data.results[0].urls[2].url)
+}
+
+var setWiki = function(data){
+  $('#wikiLink').attr('href', data.data.results[0].urls[1].url)
+}
+
+// https://gateway.marvel.com:443/v1/public/characters/Thor/comics?apikey=331a8e964a4e496298bdfa8a074e7db6
+
+//character ID range 1009301-1009726
